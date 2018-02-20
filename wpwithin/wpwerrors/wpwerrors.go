8a586a1487ed1d10e5f2047b50f775197ddef843
waldemar berendt <waldemar.berendt@mobica.com>
2018-02-20 11:20:59 +0100

    replace errors in core package

--- wpwithin/wpwerrors/wpwerrors.go
+++ wpwithin/wpwerrors/wpwerrors.go
@@ -5,18 +5,6 @@ import (
 	"strings"
 )
 
-type ErrorId int
-
-const (
-	UNKNOWN ErrorId = iota
-	NO_DATA
-	DIVISION_BY_ZERO
-	WRONG_CONFIG_PATH
-	OPEN_FILE
-	DECODE_JSON
-	CONVERT_VALUE
-)
-
 const (
 	GENERAL = "GENERAL"
 	NET     = "NET"
@@ -25,6 +13,26 @@ const (
 	MATH    = "MATH"
 	CONFIG  = "CONFIG"
 	IO      = "IO"
+	CORE    = "CORE"
+)
+
+type ErrorID int
+
+const (
+	UNKNOWN ErrorID = iota
+	NO_DATA
+	DIVISION_BY_ZERO
+	WRONG_CONFIG_PATH
+	OPEN_FILE
+	DECODE_JSON
+	CONVERT_VALUE
+	UUID_FILE_READ
+	UUID_FILE_CREATE
+	UUID_FILE_SAVE
+	PSP_CONFIG_NOT_SET
+	PARSE_DEVELOPER_ID
+	PSP_UNKNOWN
+	PSP_COLLECTION
 )
 
 type WpwError struct {
@@ -34,13 +42,20 @@ type WpwError struct {
 }
 
 var errors = [...]WpwError{
-	UNKNOWN:           {"UNKNOWN", GENERAL, "unknow error"},
-	NO_DATA:           {"NO_DATA", DATA, "lack of data"},
-	DIVISION_BY_ZERO:  {"DIVISION_BY_ZERO", MATH, "division by zero"},
-	WRONG_CONFIG_PATH: {"WRONG_CONFIG_PATH", CONFIG, "wrong path to configuration data"},
-	OPEN_FILE:         {"OPEN_FILE", CONFIG, "failed to open file"},
-	DECODE_JSON:       {"DECODE_JSON", CONFIG, "failed to decode json"},
-	CONVERT_VALUE:     {"CONVERT_VALUE", CONFIG, "failed to convert value"},
+	UNKNOWN:            {"UNKNOWN", GENERAL, "unknow error"},
+	NO_DATA:            {"NO_DATA", DATA, "lack of data"},
+	DIVISION_BY_ZERO:   {"DIVISION_BY_ZERO", MATH, "division by zero"},
+	WRONG_CONFIG_PATH:  {"WRONG_CONFIG_PATH", CONFIG, "wrong path to configuration data"},
+	OPEN_FILE:          {"OPEN_FILE", CONFIG, "failed to open file"},
+	DECODE_JSON:        {"DECODE_JSON", CONFIG, "failed to decode json"},
+	CONVERT_VALUE:      {"CONVERT_VALUE", CONFIG, "failed to convert value"},
+	UUID_FILE_READ:     {"UUID_FILE_READ", CORE, "failed to read uuid file"},
+	UUID_FILE_CREATE:   {"UUID_FILE_CREATE", CORE, "failed to create uuid file"},
+	UUID_FILE_SAVE:     {"UUID_FILE_SAVE", CORE, "failed to save uuid file"},
+	PSP_CONFIG_NOT_SET: {"PSP_CONFIG_NOT_SET", CORE, "PSP Config is not set"},
+	PARSE_DEVELOPER_ID: {"PARSE_DEVELOPER_ID", CORE, "failed to parse developerID"},
+	PSP_UNKNOWN:        {"PSP_UNKNOWN", CORE, "unknown psp"},
+	PSP_COLLECTION:     {"PSP_COLLECTION", CORE, "PSP Config collection is be set"},
 }
 
 // Error return formated error for specified error (e).
@@ -49,7 +64,7 @@ func (e WpwError) Error() error {
 }
 
 // GetError returns error for specified eid with additional data (if any) separated with comma.
-func GetError(eid ErrorId, additionalData ...string) error {
+func GetError(eid ErrorID, additionalData ...string) error {
 	if len(additionalData) == 0 {
 		return errors[eid].Error()
 	}
